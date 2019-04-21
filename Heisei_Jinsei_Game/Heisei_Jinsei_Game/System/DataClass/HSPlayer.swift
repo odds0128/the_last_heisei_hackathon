d0558d9ef561d3a1cebf8e6875e6abe9bf2e42e3
yuki <yukibochi1@gmail.com>
2019-04-21 12:03:29 +0900

    ある程度構造決め

---
+++ Heisei_Jinsei_Game/Heisei_Jinsei_Game/System/DataClass/HSPlayer.swift
@@ -0,0 +1,19 @@
+//
+//  HSPlayer.swift
+//  Heisei_Jinsei_Game
+//
+//  Created by yuki on 2019/04/21.
+//  Copyright © 2019 yuki. All rights reserved.
+//
+
+import Foundation
+
+class HSPlayer {
+    var name:String
+    var money:Int = ""
+    var familyCount:Int = 0
+    
+    init(name:String) {
+        self.name = name
+    }
+}
---
+++ Heisei_Jinsei_Game/Heisei_Jinsei_Game/View-ViewModel/MapDetail/HSMapDetailViewController.swift
@@ -0,0 +1,9 @@
+//
+//  HSMapDetailViewController.swift
+//  Heisei_Jinsei_Game
+//
+//  Created by yuki on 2019/04/21.
+//  Copyright © 2019 yuki. All rights reserved.
+//
+
+import Foundation
