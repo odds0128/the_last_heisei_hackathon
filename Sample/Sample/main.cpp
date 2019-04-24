70eb8f84889c25194b4641b8d2c3674ac31224a6
yuki <yukibochi1@gmail.com>
2019-04-24 15:49:15 +0900

    .DS_Store消し

---
+++ Sample/Sample/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sample
+//
+//  Created by yuki on 2019/04/23.
+//  Copyright © 2019 yuki. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
