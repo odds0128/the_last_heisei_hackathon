b3f4c5e412b10464dd7eaf038aa4f85cdad8c578
NishizawaShota <nishizawa@emol.jp>
2019-04-27 00:45:50 +0900

    xibに置換

--- Heisei_Jinsei_Game/Heisei_Jinsei_Game/View-ViewModel/Map/balloon/HSBalloonCustomView.swift
+++ Heisei_Jinsei_Game/Heisei_Jinsei_Game/View-ViewModel/Map/balloon/HSBalloonCustomView.swift
@@ -12,17 +12,17 @@ import UIKit
 class HSBalloonCustomView: UIView {
     
     private var view: UIView!
-    private var title: UILabel!
-    private var imageView: UIImageView!
-    private var comment: UILabel!
-    private var completionBtn: UIButton!
-    
     private var viewWidth: CGFloat!
     private var viewHeight: CGFloat!
     
+    @IBOutlet var title: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var comment: UILabel!
+    @IBOutlet var completionBtn: UIButton!
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+        loadNib()
         viewWidth = frame.width
         viewHeight = frame.height
         drawView()
@@ -30,8 +30,14 @@ class HSBalloonCustomView: UIView {
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        
+        loadNib()
     }
+    
+//    func loadNib(){
+//        let view = Bundle.main.loadNibNamed("HSBalloonCustomView.xib", owner: self, options: nil)?.first as! UIView
+//        view.frame = self.bounds
+//        self.addSubview(view)
+//    }
     
     private func drawView() {
         self.view = UIView(frame: CGRect(x: 0, y: 0, width: viewWidth, height: viewHeight))
@@ -40,27 +46,24 @@ class HSBalloonCustomView: UIView {
         HSShadow.init(layer: view.layer, offset: CGSize.zero, opacity: 0.5, radius: 10)
         addSubview(view)
         
-        drawTitle()
-        drawImageView()
-        drawComment()
-        drawCompletionBtn()
+        setTitle()
+        setImageView()
+        setComment()
+        setCompletionBtn()
     }
     
-    private func drawTitle() {
-        title = UILabel(frame: CGRect(x: 0, y: 0, width: viewWidth, height: viewHeight/9))
-        title.textAlignment = NSTextAlignment.center
+    private func setTitle() {
+        if title != nil {
+            print(title)
+        } else {
+            print("nil")
+        }
         title.text = "元号発表"
         title.layer.cornerRadius = 30
         title.clipsToBounds = true
-        
-        title.font = UIFont(name: HSFont().boldFont, size: 30)
-        title.textColor = .black
-        view.addSubview(title)
     }
     
-    private func drawImageView() {
-        imageView = UIImageView(frame: CGRect(x: (viewWidth/2) - ((viewWidth/1.1)/2), y: viewHeight/9, width: viewWidth/1.1, height: viewHeight/1.8))
-        imageView.backgroundColor = .yellow
+    private func setImageView() {
         imageView.layer.cornerRadius = 20
         imageView.layer.borderColor = HSColor().grayColor.cgColor
         imageView.layer.borderWidth = 10
@@ -68,19 +71,19 @@ class HSBalloonCustomView: UIView {
         imageView.clipsToBounds = true
         imageView.image = image
         imageView.contentMode = .scaleAspectFill
-        view.addSubview(imageView)
         
     }
     
-    private func drawComment() {
-        comment = UILabel(frame: CGRect(x: (viewWidth/2) - ((viewWidth/1.1)/2), y: viewHeight/1.5, width: viewWidth, height: viewHeight/5))
+    private func setComment() {
         comment.text = "小渕恵三官房長官が記者会見室で、\n平成と書かれた生乾きの\n2文字を掲げた。"
-        comment.numberOfLines = 3
-        comment.font = UIFont(name: HSFont().boldFont, size: 20)
-        view.addSubview(comment)
     }
     
-    private func drawCompletionBtn() {
-        
+    private func setCompletionBtn() {
+        completionBtn.layer.cornerRadius = 20
     }
+    @IBAction func completionBtnTapped(_ sender: Any) {
+        print("OKボタンがタップされた")
+    }
+    
+    
 }
