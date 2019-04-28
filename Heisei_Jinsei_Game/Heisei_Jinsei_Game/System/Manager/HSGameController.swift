bb28ae0f2a56ae5d95850d1ed2034b3e280ea20b
NishizawaShota <nishizawa@emol.jp>
2019-04-28 15:59:12 +0900

    不要なprint文など削除。

--- Heisei_Jinsei_Game/Heisei_Jinsei_Game/System/Manager/HSGameController.swift
+++ Heisei_Jinsei_Game/Heisei_Jinsei_Game/System/Manager/HSGameController.swift
@@ -39,7 +39,7 @@ class HSGameController {
         self.playerManager.didSpinWheel(for: currentPlayer, with: wheelValue)
         
         isWatingRouletteAnimation = true
-        print("gameController randomNum: \(wheelValue)")
+        
         return wheelValue
     }
     
